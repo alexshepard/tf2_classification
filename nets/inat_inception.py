761063a9a8b2e8064373fd9b6fd07aff5f07ffca
“Alex <alex@meatfreezer.com>
2019-10-13 15:25:54 -0700

    Revert "dual-output classification"
    this isn't really working, let's do this step by step
    
    This reverts commit 2f4328c9e72b850ebc58deb6ccbd96a47e983a91.

--- nets/inat_inception.py
+++ nets/inat_inception.py
@@ -5,43 +5,33 @@ import tensorflow as tf
 # head but this will get the dual-head factorized
 # stuff
 
-def compiled_model(img_shape, num_classes):
+def compiled_model(img_shape, image_batch, num_classes):
     base_model = tf.keras.applications.InceptionV3(
         input_shape=img_shape,
         include_top=False,
         weights='imagenet'
     )
 
+    feature_batch = base_model(image_batch)
     base_model.trainable = True
 
     # add a classification head
-    x = tf.keras.layers.GlobalAveragePooling2D(name='pool1')(base_model.output)
-    pred1 = tf.keras.layers.Dense(
-        num_classes,
-        activation='softmax',
-        name='head1'
-    )(x)
-
-    # add a second classification head
-    # currently identical to the first classification head
-    # but this will shortly be "factorized"/"compressed"
-    x = tf.keras.layers.GlobalAveragePooling2D(name='pool2')(base_model.output)
-    pred2 = tf.keras.layers.Dense(
-        num_classes,
-        activation='softmax',
-        name='head2'
-    )(x)
+    global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+    feature_batch_average = global_average_layer(feature_batch)
+    prediction_layer = tf.keras.layers.Dense(num_classes, activation='softmax')
+    prediction_batch = prediction_layer(feature_batch_average)
     
-    # build a dual-output/dual-head model
-    model = tf.keras.models.Model(
-        base_model.input,
-        [pred1, pred2]
-    )
+    # append the classification head to the base model
+    model = tf.keras.Sequential([
+        base_model,
+        global_average_layer,
+        prediction_layer
+    ])
 
     base_learning_rate = 0.0001
     model.compile(
         optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
-        loss=['categorical_crossentropy', 'categorical_crossentropy'],
+        loss='categorical_crossentropy',
         metrics=['accuracy']
     )
 
